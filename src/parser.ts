300d836f41dbbd6b62b028674dce9613264f0191
Yury <72691412+cheatsnake@users.noreply.github.com>
2021-12-20 13:16:58 +0300

    fix objects category

--- src/parser.ts
+++ src/parser.ts
@@ -6,9 +6,9 @@ const EmojiCategories = {
     Smileys_People: "smileys_and_people.cfm",
     Animals_Nature: "animals_and_nature.cfm",
     Food_Drink: "food_and_drink.cfm",
-    Activity: "activities.cfm",
+    Activities: "activities.cfm",
     Travel_Places: "travel_and_places.cfm",
-    Objects: "activities.cfm",
+    Objects: "objects.cfm",
     Symbols: "symbols.cfm",
     Flags: "flags.cfm",
 };
