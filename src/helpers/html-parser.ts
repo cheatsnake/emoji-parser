93553edf7b0ba04f8779fedce516e60858ec77b3
Yury <72691412+cheatsnake@users.noreply.github.com>
2021-12-20 10:29:57 +0300

    fix bug with zero index

--- src/helpers/html-parser.ts
+++ src/helpers/html-parser.ts
@@ -15,12 +15,12 @@ export default async function parseEmojiData(
         $("ul.list-unstyled.charset-ref")
             .children()
             .each((i, elem) => {
-                emojiData[i - 1] = [$(elem).find("h2").text()];
+                emojiData[i] = [$(elem).find("h2").text()];
                 $(elem)
                     .find("ul")
                     .children()
                     .each((i2, elem2) => {
-                        emojiData[i - 1].push($(elem2).children().attr("href"));
+                        emojiData[i].push($(elem2).children().attr("href"));
                     });
             });
         return emojiData;
